2b02c75ad04a17c13a514a717ca83655d2672728
blrysafy <safyishanov@gmail.com>
2025-11-18 17:08:48 +0500

    Add solution from module4

---
+++ module4/module4solutions/module4solutions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class MainClass
+{
+    static void Main(string[] args)
+
+    {
+
+
+        //    const string Myname = "Jane";
+
+        //    Console.WriteLine(Myname);
+
+        //    Console.WriteLine("\t  Hey, world");
+        //    Console.WriteLine("\t  I`m 29 years old");
+        //    Console.WriteLine("\t  My name is  \nJane");
+
+
+        //solution 3.2.8
+
+        Console.WriteLine("\u0023");
+    
+    
+    }
+
+}
+

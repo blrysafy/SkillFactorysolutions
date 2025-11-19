929384bb816f081a8a9f7c735a5da591433f34e0
blrysafy <safyishanov@gmail.com>
2025-11-19 10:06:04 +0500

    update module4_33.cs

--- module4/module4solutions/module4_33.cs
+++ module4/module4solutions/module4_33.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string MyName = "Jane";
+byte MyAge = 27;
+bool HaveIPet = true;
+double MyShoeSize = 37.5;
+
+Console.WriteLine("My name is " + MyName);
+Console.WriteLine("MyAge " + MyAge);
+Console.WriteLine("Do I have a pet? " + HaveIPet);
+Console.WriteLine("My shoe size is " + MyShoeSize);

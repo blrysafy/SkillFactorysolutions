64dbb4252ac92cb505f2e726f607b67ce2ed28bd
blrysafy <safyishanov@gmail.com>
2025-11-22 11:43:06 +0500

    Add solution to module4_3.7

--- module4/module4solutions/module4solutions/DaysOfWeek.cs
+++ module4/module4solutions/module4solutions/DaysOfWeek.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 namespace module4solutions
 {
+    enum Semaphore
+    {
+        Red = 100,
+        Yellow =200,
+        Green = 300
+    }
+} 
 
 
 
-}
 
--- module4/module4solutions/module4solutions/Program.cs
+++ module4/module4solutions/module4solutions/Program.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 class MainClass {
   public static void Main (string[] args) {
-    //DaysOfWeek MyFavoriteDay;
+        //DaysOfWeek MyFavoriteDay;
 
-    //MyFavoriteDay = DaysOfWeek.Friday;
+        //MyFavoriteDay = DaysOfWeek.Friday;
 
-    //Console.WriteLine(MyFavoriteDay);
+        //Console.WriteLine(MyFavoriteDay);
 
-  }
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        byte age = (byte)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.Write("Enter your birthdate: ");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthdate is {0}", birthdate);
+        Console.ReadKey();
+    }
 }
 
 //enum DaysOfWeek : byte
 //{
-//  Tuesday,
-//  Monday,
-//  Wednesday,
-//  Friday
+//    Tuesday,
+//    Monday,
+//    Wednesday,
+//    Friday
 //}
 

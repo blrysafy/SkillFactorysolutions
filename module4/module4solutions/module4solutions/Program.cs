2c0e9eddd555835b12d2f2798f30bb1933049614
blrysafy <safyishanov@gmail.com>
2025-11-25 09:51:35 +0500

    Add choose_color with switch operator

--- module4/module4solutions/module4solutions/Program.cs
+++ module4/module4solutions/module4solutions/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-class MainClass {
-  public static void Main (string[] args) {
+class MainClass 
+{
+  public static void Main (string[] args) 
+  {
         //DaysOfWeek MyFavoriteDay;
 
         //MyFavoriteDay = DaysOfWeek.Friday;
@@ -8,16 +10,51 @@ class MainClass {
         //Console.WriteLine(MyFavoriteDay);
 
 
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        byte age = (byte)int.Parse(Console.ReadLine());
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-        Console.Write("Enter your birthdate: ");
-        var birthdate = Console.ReadLine();
-        Console.WriteLine("Your birthdate is {0}", birthdate);
-        Console.ReadKey();
+        //Console.Write("Enter your name: ");
+        //string name = Console.ReadLine();
+        //Console.Write("Enter your age: ");
+        //byte age = (byte)int.Parse(Console.ReadLine());
+        //Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        //Console.Write("Enter your birthdate: ");
+        //var birthdate = Console.ReadLine();
+        //Console.WriteLine("Your birthdate is {0}", birthdate);
+        //Console.ReadKey();
+
+
+        //solution4.1
+
+
+        Console.WriteLine("Print your favorite color in lowcase");
+
+        var color = Console.ReadLine();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+                
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+
+            default:
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                break;
+
+
+        }
+        Console.ReadKey;
+
     }
+    
 }
 
 //enum DaysOfWeek : byte
